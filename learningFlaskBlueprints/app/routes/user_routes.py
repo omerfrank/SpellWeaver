a01c73b2160721985f22e9e65e33169a9ce9a5a4
omerF <ziv@arr.com>
2025-10-29 23:16:25 +0200

    start working on project structure

--- learningFlaskBlueprints/app/routes/user_routes.py
+++ learningFlaskBlueprints/app/routes/user_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 
 # Create a blueprint instance
-user_bp = Blueprint('user', __name__, template_folder='../templates')
+user_bp = Blueprint('user', __name__, template_folder='../templates',static_folder='../static')
 
 @user_bp.route('/users')
 def get_users():
